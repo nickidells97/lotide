37a2dcc09dd9dce3562b40d27c27ad52681684b6
Nick Dellar <ndelfern@live.ca>
2022-08-22 23:11:04 +0000

    final Code

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed:" + actual + " === " + expected);
+  } else
+    console.log("🛑🛑🛑 Assertion Failed:" + actual + " !== " + expected);
+  return;
+};
+
+const countLetters = function(sentence) {
+  let array = sentence.split('')
+  const results = {}; 
+    for (const item of array) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+      delete results[' '];
+  }
+  console.log(results);
+}
+
+
+countLetters("lighthouse in the house")
