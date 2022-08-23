4c218b349d48c1fc1aa876e402511c85c1966ff1
Nick Dellar <ndelfern@live.ca>
2022-08-23 03:47:37 +0000

    Final Code

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed:" + actual + " === " + expected);
+  } else
+    console.log("🛑🛑🛑 Assertion Failed:" + actual + " !== " + expected);
+  return;
+};
+
+const findKeyByValue = function(genre, show) {
+  let findKeys = Object.keys(genre);
+  for (let key of findKeys) {
+    if (genre[key] === show) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
