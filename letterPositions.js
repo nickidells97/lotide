617188ce2472ee106d801173ce84e8aec698ba19
Nick Dellar <ndelfern@live.ca>
2022-08-23 18:47:24 +0000

    Final Code

---
+++ letterPositions.js
@@ -0,0 +1,34 @@
+const eqArrays = function(arr1, arr2) {
+  if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
+    return true
+  } else {
+    return false
+  }
+}
+
+ const assertArraysEqual = function(actual, expected) {
+  if (JSON.stringify(actual) === JSON.stringify(expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  return;
+ }
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i].trim()
+    if (letter) {
+      if (results[letter]) {
+        results[letter].push(i)
+      } else {
+        results[letter] = [i];
+      }
+    }
+  }
+  return results;
+}
+
+//letterPositions("lighthouse in the house")
+
+assertArraysEqual(letterPositions("hello").e, [1]);
