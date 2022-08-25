974fcd969e05cf0ae63b78ec6304418fe98ace76
Nick Dellar <ndelfern@live.ca>
2022-08-25 03:48:28 +0000

    Final code - should code check with mentor

---
+++ map.js
@@ -0,0 +1,29 @@
+const eqArrays = function(arr1, arr2) {
+  return Array.isArray(arr1) && Array.isArray(arr2) &&
+        arr1.length === arr2.length &&
+        arr1.every((val, index) => val === arr2[index]);
+};
+
+ const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+ }
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't'])
+assertArraysEqual([results1[3], results1[4]], ['m', 't'])
+assertArraysEqual([results1[2], results1[1]], ['t', 'c'])
