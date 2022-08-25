5b140052fb77a92dc0e755ba057c2801b65b1e37
Nick Dellar <ndelfern@live.ca>
2022-08-25 00:03:56 +0000

    Fixed code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,23 @@
 const eqArrays = function(arr1, arr2) {
-  if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
-    return true
-  } else {
-    return false
-  }
+  if(Array.isArray(arr1) && Array.isArray(arr2)) {
+    if (arr1.length === arr2.length) {
+      if (arr1.every((val, index) => val === arr2[index])) {
+        return true
+      } else {
+        return false
+      }
+    }
+  } 
 }
 
  const assertArraysEqual = function(actual, expected) {
-  if (JSON.stringify(actual) === JSON.stringify(expected)) {
+  if (eqArrays) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   return;
  }
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]));
 
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]));
