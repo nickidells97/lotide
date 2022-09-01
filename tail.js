60b9c8991cc145fb436d983e911512b5d75f30ca
Nick Dellar <ndelfern@live.ca>
2022-09-01 18:41:06 +0000

    Refactored and modulated

--- tail.js
+++ tail.js
@@ -1,19 +1,7 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  return;
-};
-
 const tail = function(array) {
-  let last = array.splice[0];
+  let last = array.slice(1);
   return last;
 };
 
-// TEST CODE
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-//
+module.exports = tail;
+
