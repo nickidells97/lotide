7074a948d7fdbad172cc7d56fb4dcebcc402488b
Nick Dellar <ndelfern@live.ca>
2022-09-01 19:20:50 +0000

    Refactored and modular

--- middle.js
+++ middle.js
@@ -1,29 +1,15 @@
-const eqArrays = function(arr1, arr2) {
-  return Array.isArray(arr1) && Array.isArray(arr2) &&
-        arr1.length === arr2.length &&
-        arr1.every((val, index) => val === arr2[index]);
-};
-
- const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
- }
-
- const middle = function(array) {
+const middle = function(array) {
   let results = [];
   if (array.length % 2 === 0) {
     results = new Array(2);
-    results[0] = array.length/2 
-    results[1] = array.length/2 +1
+    results[0] = array.length / 2;
+    results[1] = array.length / 2 + 1;
   } else {
     results = new Array(1);
-    results[0] = array.length/2
+    results[0] = array.length / 2;
   }
   return results;
- }
+};
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]) // => [3, 4]
+module.exports = middle;
+
