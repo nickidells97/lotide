2f1ca8cf5337fd1b23c967d2dc7fba24e00f31d1
Nick Dellar <ndelfern@live.ca>
2022-09-01 18:52:53 +0000

    Refactored and modular

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  return;
-};
 const eqArrays = function(arr1, arr2) {
   return Array.isArray(arr1) && Array.isArray(arr2) &&
         arr1.length === arr2.length &&
         arr1.every((val, index) => val === arr2[index]);
 };
 
-// test code
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+module.exports = eqArrays;
