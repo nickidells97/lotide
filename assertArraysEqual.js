3d638f585838a82a759166634cfbe7e84ebd18d3
Nick Dellar <ndelfern@live.ca>
2022-09-01 19:12:53 +0000

    Refactored and modular

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,11 @@
-const eqArrays = function(arr1, arr2) {
-  return Array.isArray(arr1) && Array.isArray(arr2) &&
-        arr1.length === arr2.length &&
-        arr1.every((val, index) => val === arr2[index]);
-};
-
- const assertArraysEqual = function(actual, expected) {
+const eqArrays = require('./eqArrays');
+ 
+const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
- }
+};
 
- assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
- assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
- 
- assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
- assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+module.exports = assertArraysEqual;
