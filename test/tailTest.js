9411f2a0568fcb8c7a04ad43cd55615e94295157
Nick Dellar <ndelfern@live.ca>
2022-09-02 04:53:28 +0000

    Mocha and Chai insertion

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// TEST CODE
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-
-
+describe("#head", () => {
+  it("returns Lighthouse, Labs for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs", ]);
+  });
+  it("returns mad, happy, scared for ['sad', 'mad', 'happy', 'scared']", () => {
+    assert.deepEqual(tail(["sad", "mad", "happy", "scared"]), ["mad", "happy", "scared"]);
+  });
+  it("returns ' ' for ['banana']", () => {
+    assert.deepEqual(tail(["banana"]), []);
+  });
+});
